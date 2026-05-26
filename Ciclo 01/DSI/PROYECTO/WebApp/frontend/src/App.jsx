d3c38f8daaaafc06c87babda90dbae180a2fbc98
LuisVasquez <vm15037@ues.edu.sv>
2026-05-25 19:48:34 -0600

    i

--- Ciclo 01/DSI/PROYECTO/WebApp/frontend/src/App.jsx
+++ Ciclo 01/DSI/PROYECTO/WebApp/frontend/src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import { Container, Row, Col, Navbar } from 'react-bootstrap';
 import CitasLista from './components/CitasLista';
-import FormularioCita from './components/FormularioCita';
 import { useCitas } from './hooks/useCitas';
 
 export default function App() {
   const [citaAEditar, setCitaAEditar] = useState(null);
-  
+
   // Consumimos el estado y funciones globales de nuestro custom hook
-  const { 
-    citas, 
-    cargando, 
-    agendarNuevaCita, 
-    modificarCita, 
-    cancelarCita 
+  const {
+    citas,
+    cargando,
+    agendarNuevaCita,
+    modificarCita,
+    cancelarCita
   } = useCitas();
 
   const handleCitaGuardada = () => {
@@ -23,13 +22,13 @@ export default function App() {
   return (
     <div style={{ backgroundColor: '#f4f6f9', minHeight: '100vh' }}>
       <Navbar bg="dark" variant="dark" className="px-4">
-        <Navbar.Brand>🦷 DentalCare System</Navbar.Brand>
+        <Navbar.Brand href="/"> 🦷 DentalCare System</Navbar.Brand>
       </Navbar>
 
       <Container fluid className="p-4">
         <Row>
-          <Col md={4}>
-            <div className="bg-white p-4 rounded shadow-sm">
+          {/* <Col md={4}> */}
+            {/* <div className="bg-white p-4 rounded shadow-sm">
               <h4>{citaAEditar ? "Modificar Cita" : "Agendar Nueva Cita"}</h4>
               <hr />
               <FormularioCita
@@ -38,16 +37,16 @@ export default function App() {
                 agendarNuevaCita={agendarNuevaCita}
                 modificarCita={modificarCita}
               />
-            </div>
-          </Col>
+            </div> */}
+          {/* </Col> */}
 
-          <Col md={8}>
+          <Col md={12}>
             <div className="bg-white p-4 rounded shadow-sm">
-              <CitasLista 
-                citas={citas} 
-                cargando={cargando} 
-                onCancelar={cancelarCita} 
-                onEditarSeleccionado={setCitaAEditar} 
+              <CitasLista
+                citas={citas}
+                cargando={cargando}
+                onCancelar={cancelarCita}
+                onEditarSeleccionado={setCitaAEditar}
               />
             </div>
           </Col>
